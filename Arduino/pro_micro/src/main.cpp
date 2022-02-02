2dc283aa3729783f5746d3f2ec2753c78524d069
Harmony <harmonised7@gmail.com>
2022-02-02 13:47:37 +0000

    Serial to EEPROM for every button, EEPROM to Keyboard for every button

--- Arduino/pro_micro/src/main.cpp
+++ Arduino/pro_micro/src/main.cpp
@@ -9,7 +9,7 @@
 
 #include<Keyboard.h>
 
-#define SERIAL_LENGTH_MAX 16
+#define SERIAL_LENGTH_MAX 512
 #define EEPROM_ADDRESS_IC2 0x50
 
 #define DEBUG_1
@@ -24,18 +24,19 @@
 
 //int rows, cols, buttonCount;
 
-long lastSend;
+unsigned long lastSend;
+boolean button, wasButton;
 
 void processSerial(const char inputChars[]);
 void debugPrint();
 void writeData(uint16_t address, byte data);
-void writeData(uint16_t address, const byte data[], uint16_t size);
-void writeData(uint16_t address, const char data[], uint16_t size);
+void writeData(uint16_t address, const byte data[], int size);
+void writeData(uint16_t address, const char data[], int size);
 byte readData(uint16_t address);
-void readData(uint16_t address, byte buffer[], uint16_t size);
+void readData(uint16_t address, byte buffer[], int size);
 void greetEeprom(uint16_t address);
 void saveButtonData(unsigned buttonIndex, const byte data[]);
-void readButtonData(unsigned buttonIndex, byte buffer[], uint16_t size);
+void readButtonData(unsigned buttonIndex, byte buffer[], int size);
 void readButtonData(unsigned buttonIndex, byte buffer[]);
 uint16_t readButtonSize(unsigned buttonIndex);
 void sendHID(const char chars[]);
@@ -43,7 +44,6 @@ void sendHID(const char chars[], uint16_t delay);
 uint16_t getButtonAddress(uint8_t buttonIndex);
 uint16_t getButtonSizeAddress(unsigned buttonIndex);
 uint16_t getStringLength(const char inputChars[]);
-uint16_t getStringLength(const byte input[]);
 
 uint16_t merge8To16(uint8_t a, uint8_t b);
 
@@ -55,10 +55,12 @@ void setup()
     Serial.setTimeout(200);
 
     Wire.begin();
-    writeData(0, "32511523", 8);
+//    writeData(0, "32511523", 8);
 
-    DDRB |= (1 << 5);
-    PORTB &= ~(1 << 5);
+    DDRF |= (1 << 5);
+//    PORTB &= ~(1 << 2);
+
+//    pinMode(A10, INPUT);
 }
 
 void loop()
@@ -68,15 +70,40 @@ void loop()
 //        readData(0, debugMsg, 8);
 //        Serial.println(millis());
     }
+
+    button = PINF & 1 << 5;
+    if(button && !wasButton)
+    {
+        Serial.println("button!");
+        int size = readButtonSize(1);
+        byte hid[size];
+        Serial.println(size);
+        readButtonData(1, hid);
+        sendHID(reinterpret_cast<const char*>(hid), 25);
+    }
+    wasButton = button;
+
+    if(lastSend + 1523 < millis())
+    {
+        Serial.println("I'm alive");
+//        Serial.print("button: ");
+//        Serial.println(button);
+//        Serial.print("analogRead(A2): ");
+//        Serial.println(analogRead(A2));
+//        Serial.println();
+        lastSend = millis();
+    }
+
     while (Serial.available())
     {
         PORTB |= (1 << 5);
         Serial.println("Why Hello There");
         String inputString = Serial.readString();
-        uint16_t size = inputString.length()+1;
+        int size = inputString.length()+1;
         char inputChars[size];
         inputString.toCharArray(inputChars, size);
-//        processSerial(inputString);
+//        delete &inputString;
+        processSerial(inputChars);
         PORTB &= ~(1 << 5);
     }
 }
@@ -86,17 +113,12 @@ void processSerial(const char inputChars[])
 #ifdef DEBUG_2
     Serial.println("chars:");
     Serial.println(inputChars);
-    uint16_t size = getStringLength(inputChars);
-//    for(int i = 0; i < size; ++i)
-//    {
-//        Serial.print(i);
-//        Serial.print(": ");
-//        Serial.println(inputChars[i]);
-//    }
+    int size = getStringLength(inputChars);
     Serial.print("length: ");
     Serial.println(size);
 #endif
-
+    saveButtonData(1, reinterpret_cast<const byte*>(inputChars));
+    return;
     DynamicJsonDocument doc(SERIAL_LENGTH_MAX);
 #ifdef DEBUG_1
     Serial.print("capacity: ");
@@ -129,34 +151,44 @@ void processSerial(const char inputChars[])
     Serial.println("Parsing");
 #endif
 
-    Serial.println(doc.containsKey("info"));
-    if(doc.containsKey("info"))
+
+    if(doc.containsKey("i") && doc.containsKey("t"))
     {
-//        rows = doc["info"].getMember("row").as<int>();
-//        cols = doc["info"].getMember("col").as<int>();
-//        buttonCount = rows * cols;
+        uint8_t buttonIndex = doc.getMember("i").as<int>();
+        char type = doc.getMember("t").as<byte>();
+        Serial.print("button index: ");
+        Serial.println(buttonIndex);
+        Serial.print("type: ");
+        Serial.println(type);
+        switch(type)
+        {
+            case 'b':
+                Serial.println("Parsing Button!!");
+                break;
+        }
+
 
 #ifdef DEBUG_2
         Serial.println("info parsed");
 #endif
     }
 
-    if(doc.containsKey("buttons"))
-    {
-        JsonObject buttonsJson = json["buttons"];
-        for(int i = 0; i < BUTTON_COUNT; i++)
-        {
-            String key = String(i);
-            if(buttonsJson.containsKey(key))
-            {
-                #ifdef DEBUG_2
-                JsonObject buttonJson = buttonsJson[key];
-                Serial.print("parsed button ");
-                Serial.println(i);
-                #endif
-            }
-        }
-    }
+//    if(doc.containsKey("buttons"))
+//    {
+//        JsonObject buttonsJson = json["buttons"];
+//        for(int i = 0; i < BUTTON_COUNT; i++)
+//        {
+//            String key = String(i);
+//            if(buttonsJson.containsKey(key))
+//            {
+//                #ifdef DEBUG_2
+//                JsonObject buttonJson = buttonsJson[key];
+//                Serial.print("parsed button ");
+//                Serial.println(i);
+//                #endif
+//            }
+//        }
+//    }
 
 #ifdef DEBUG_1
     debugPrint();
@@ -175,13 +207,13 @@ void debugPrint()
 //    Serial.println(cols);
 }
 
-void writeData(uint16_t address, const char data[], uint16_t size)
+void writeData(uint16_t address, const char data[], int size)
 {
     while(size-- >= 0)
         writeData(address++, *data++);
 }
 
-void writeData(uint16_t address, const byte data[], uint16_t size)
+void writeData(uint16_t address, const byte data[], int size)
 {
     while(size-- >= 0)
         writeData(address++, *data++);
@@ -201,13 +233,13 @@ void writeData(uint16_t address, byte data)
     delay(5);
 }
 
-void readData(uint16_t address, byte buffer[], uint16_t size)
+void readData(uint16_t address, byte buffer[], int size)
 {
     while(size-- >= 0)
         *buffer++ = readData(address++);
 }
 
-void readData(uint16_t address, char buffer[], uint16_t size)
+void readData(uint16_t address, char buffer[], int size)
 {
     while(size-- >= 0)
         *buffer++ = (char) readData(address++);
@@ -238,15 +270,6 @@ uint16_t getStringLength(const char inputChars[])
     return i;
 }
 
-uint16_t getStringLength(const byte input[])
-{
-    uint16_t i = 0;
-    unsigned const int arraySize = sizeof(input);
-    while(input[i] != '\0' && i < arraySize)
-        ++i;
-    return i;
-}
-
 void sendHID(const char chars[])
 {
     const uint16_t length = getStringLength(chars);
@@ -256,6 +279,10 @@ void sendHID(const char chars[])
 
 void sendHID(const char chars[], const uint16_t gapMs)
 {
+#ifdef DEBUG_1
+    Serial.print("Sending HID: ");
+    Serial.println(chars);
+#endif
     const uint16_t length = getStringLength(chars);
     for(int i = 0; i < length; ++i)
     {
@@ -274,10 +301,15 @@ uint16_t getButtonAddress(uint8_t buttonIndex)
 
 void saveButtonData(unsigned buttonIndex, const byte data[])
 {
-    writeData(getButtonAddress(buttonIndex), data, getStringLength(data));
+    int size = (int) getStringLength(reinterpret_cast<const char*>(data));
+    writeData(getButtonAddress(buttonIndex), data, size);
+    unsigned int buttonSizeAddress = getButtonSizeAddress(buttonIndex);
+    //Save button data length as two bytes
+    writeData(buttonSizeAddress, size & 255);
+    writeData(buttonSizeAddress+1, size >> 8);
 }
 
-void readButtonData(unsigned buttonIndex, byte buffer[], uint16_t size)
+void readButtonData(unsigned buttonIndex, byte buffer[], int size)
 {
     readData(getButtonAddress(buttonIndex), buffer, size);
 }
@@ -289,14 +321,15 @@ void readButtonData(unsigned buttonIndex, byte buffer[])
 
 uint16_t readButtonSize(unsigned buttonIndex)
 {
-    uint16_t readSize = getButtonSizeAddress(getButtonSizeAddress(buttonIndex));
-    return readSize < BUTTON_ALLOCATION ? readSize : BUTTON_ALLOCATION;
+    uint16_t address = getButtonSizeAddress(buttonIndex);
+    uint16_t readSize = merge8To16(readData(address),
+                                   readData(address + 1));
+    return readSize < SERIAL_LENGTH_MAX ? readSize : SERIAL_LENGTH_MAX;
 }
 
 uint16_t getButtonSizeAddress(unsigned buttonIndex)
 {
-    return merge8To16(readData(buttonIndex * BUTTON_INFO_SIZE_ALLOCATION),
-                      readData(buttonIndex * BUTTON_INFO_SIZE_ALLOCATION + 1));
+    return buttonIndex * BUTTON_INFO_SIZE_ALLOCATION;
 }
 
 uint16_t merge8To16(uint8_t a, uint8_t b)
